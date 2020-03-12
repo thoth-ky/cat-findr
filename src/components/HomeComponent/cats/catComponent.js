090c8ac8db2a066d9ca76b37ab0ef3c619069011
thoth-ky <28805113+thoth-ky@users.noreply.github.com>
2020-03-13 01:00:18 +0300

    add detaisl to cat page

--- src/components/HomeComponent/cats/catComponent.js
+++ src/components/HomeComponent/cats/catComponent.js
@@ -1,5 +1,7 @@
 import React, {useEffect} from 'react';
 import Card from 'react-bootstrap/Card'
+import ListGroup from 'react-bootstrap/ListGroup'
+import isEmpty from 'is-empty';
 
 function CatComponent({image={}, display={}}){
   const {
@@ -10,19 +12,58 @@ function CatComponent({image={}, display={}}){
     breeds=[],
   } = image;
 
+  const { 
+    name: breedName = 'Not Available',
+    description = 'Not Available',
+  } = (breeds && breeds[0]) || {}
 
-  const { id: breedID, name: breedName, description, origin, life_span, } = {} || breeds && breeds[0]
+
   const { expand = false } = display;
 
-  
+  if (isEmpty(image)){
+    return (
+      <Card.Text>
+        Loading Image...
+      </Card.Text>
+    )
+  }
+
+  const ExtraDetails = (breed) => {
+    const { 
+      id: breedID,
+      origin,
+      life_span: lifeSpan,
+      adaptability,
+      affection_level: affectionLevel,
+      child_friendly: childFriendly,
+      dog_friendly: dogFriendly,
+      energy_level: energyLevel,
+      grooming,
+    } = (breeds && breeds[0]) || {}
+    return (
+      <Card key={breedID}>
+        <ListGroup variant="flush">
+          <ListGroup.Item>Origin          : {origin}</ListGroup.Item>
+          <ListGroup.Item>Life Span       : {lifeSpan}</ListGroup.Item>
+          <ListGroup.Item>Dog Friendly    : {dogFriendly}</ListGroup.Item>
+          <ListGroup.Item>Adaptability    : {adaptability}</ListGroup.Item>
+          <ListGroup.Item>Child Friendly  : {childFriendly}</ListGroup.Item>
+          <ListGroup.Item>Affection Level : {affectionLevel}</ListGroup.Item>
+          <ListGroup.Item>Energy Level    : {energyLevel}</ListGroup.Item>
+          <ListGroup.Item>Grooming        : {grooming}</ListGroup.Item>
+        </ListGroup>
+      </Card>
+    )
+  }
   return (
     <Card  style={{width: '40rem'}} key={id} className="text-center">
       <Card.Img variant="top" width='40rem'  height='400px' src={url}/>
       <Card.Body>
-        <Card.Text>
-          <b>Breed : {breedName}</b>
+        <Card.Text as='div'>
+          <b>Breed</b> : {breedName}
           <br/>
-          <i>Description : {description}</i>
+          <b>Description</b> : <i>{description}</i>
+          {!isEmpty(breeds) && expand &&  <ExtraDetails breed={breeds[0]}/>}
         </Card.Text>
       </Card.Body>
     </Card>
