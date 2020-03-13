5993ad2b9f84fb0f6f2e5195366db62522e8daab
thoth-ky <28805113+thoth-ky@users.noreply.github.com>
2020-03-13 14:28:41 +0300

    add reducer and action

---
+++ src/components/FavsComponent/actions/FavActions.js
@@ -0,0 +1,12 @@
+import { getFromApi } from '../../../utils/api';
+
+const getFavCatAction = (dispatch, favID, params = {}) => {
+  const actionData = {
+    type: 'ADD_FAV_CATS',
+  };
+  getFromApi(`images/${favID}`, dispatch, actionData, params);
+};
+
+export {
+  getFavCatAction,
+};
---
+++ src/components/FavsComponent/reducers/index.js
@@ -0,0 +1,1 @@
+export { favCatReducer } from './favCatReducer';
